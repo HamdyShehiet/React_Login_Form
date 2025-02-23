6ab404081f69bdc373c9aec3524f3cf91c5024fa
Hamdy <tantaacademyt196@gmail.com>
2025-02-23 17:02:56 +0000

    Add: Login Form Functionality

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 function Login() {
+
+  const [userame, setUserame] = useState("");
+  const [password, setPassword] = useState("");
+
+  function Login (){
+    if(userame !== "" && password !== ""){
+      console.log(userame);
+      console.log(password);
+    }
+  }
+
   return (
     <div className="container flex align-center justify-center">
-        <form className="flex flex-column">
+        <form onSubmit={(e)=>{e.preventDefault()}} className="flex flex-column">
         <h1>Login</h1>
         <div className="username-field row">
-            <input type="text"  name="username" id="username" required placeholder="Username" />
+            <input onChange={(e)=>{setUserame(e.target.value)}} type="text"  name="username" id="username" required placeholder="Username" />
             <i className="fa-solid fa-user"></i>
         </div>
         <div className="password-field row">
-            <input type="password" name="password" id="password" required placeholder="Password" />
+            <input onChange={(e)=>{setPassword(e.target.value)}} type="password" name="password" id="password" required placeholder="Password" />
             <i className="fa-solid fa-lock"></i>
         </div>
         <div className="Instruction-field flex align-center">
@@ -16,12 +29,12 @@ function Login() {
             <input type="checkbox"  name="remember" value="remember" id="remember" />
             <label htmlFor="remember">Remember me</label>
         </div>
-        <a href="/" target="_blank" title="Forgot Password?">Forgot password ?</a>
+        <a href="/"  title="Forgot Password?">Forgot password ?</a>
         </div>
-        <input type="submit" value="Login" />
+        <input type="submit" onClick={Login} value="Login" />
         <div className="new-account flex align-center justify-center">
             <p>Don't have an account ?</p>
-            <a href="/" target="_blank" title="Register">Register</a>
+            <a href="/"  title="Register">Register</a>
         </div>
     </form>
     </div>
